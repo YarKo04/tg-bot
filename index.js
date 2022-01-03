5ab24c1a083b7f4756a7ab2e40ef3513c5e484b2
Yaroslav Kokoshko <85609406+YarKo04@users.noreply.github.com>
2022-01-03 20:30:47 +0200

    basic structure

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
-const tgApi = require('node-telegram-bot-api')
+const telegramApi = require('node-telegram-bot-api');
+const config = require('config');
 
-const token = ''
+const token = config.get('token');
 
-const bot = new tgApi(token, {polling: true})
+const bot = new telegramApi(token, {
+  polling: true,
+});
 
-bot.on('message', msg => {
-    console.log(msg)
-})
+const begin = () => {
+  bot.on('polling_error', console.log);
+
+  bot.setMyCommands([
+    {
+      command: '/start',
+      description: 'Greeting',
+    },
+    {
+      command: '/info',
+      description: 'Information',
+    },
+  ]);
+
+  bot.on('message', async (msg) => {
+    console.log(msg);
+    const user = msg.from.first_name;
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if (text === '/start') {
+      await bot.sendMessage(
+        chatId,
+        'Welcome to Smart-Bot! Hope that we can make your life easier!'
+      );
+      return bot.sendSticker(
+        chatId,
+        'https://tlgrm.ru/_/stickers/88e/586/88e586f0-4299-313f-bedb-ef45c7710422/1.webp'
+      );
+    }
+
+    if (text === '/info') {
+      return bot.sendMessage(chatId, `Your name is:  ${user}`);
+    }
+    return bot.sendMessage(chatId, 'I dont understand you :( ');
+  });
+};
+
+begin();
